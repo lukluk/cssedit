99e0ff436c08f23d0fb030326211e6edb3c3ff3e
David Stewart <djranger1@gmail.com>
2011-09-20 19:45:51 -0700

    Move FileIO to its own file

--- chrome/content/cssedit/cssedit.js
+++ chrome/content/cssedit/cssedit.js
@@ -3,137 +3,10 @@ var jQuery, c, hints, match_colors;
 var setup = true;
 var panelName = 'CSSEdit';
 var DomUtils = FBL.CCSV("@mozilla.org/inspector/dom-utils;1", "inIDOMUtils");
-var FileIO = {
+var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+	.getService(Components.interfaces.mozIJSSubScriptLoader);
 
-		localfileCID  : '@mozilla.org/file/local;1',
-		localfileIID  : Components.interfaces.nsILocalFile,
-
-		finstreamCID  : '@mozilla.org/network/file-input-stream;1',
-		finstreamIID  : Components.interfaces.nsIFileInputStream,
-
-		foutstreamCID : '@mozilla.org/network/file-output-stream;1',
-		foutstreamIID : Components.interfaces.nsIFileOutputStream,
-
-		sinstreamCID  : '@mozilla.org/scriptableinputstream;1',
-		sinstreamIID  : Components.interfaces.nsIScriptableInputStream,
-
-		suniconvCID   : '@mozilla.org/intl/scriptableunicodeconverter',
-		suniconvIID   : Components.interfaces.nsIScriptableUnicodeConverter,
-
-		open   : function(path) {
-			try {
-				var file = Components.classes[this.localfileCID]
-								.createInstance(this.localfileIID);
-				file.initWithPath(path);
-				return file;
-			}
-			catch(e) {
-				return false;
-			}
-		},
-
-		read   : function(file, charset) {
-			try {
-				var data     = new String();
-				var fiStream = Components.classes[this.finstreamCID]
-									.createInstance(this.finstreamIID);
-				var siStream = Components.classes[this.sinstreamCID]
-									.createInstance(this.sinstreamIID);
-				fiStream.init(file, 1, 0, false);
-				siStream.init(fiStream);
-				data += siStream.read(-1);
-				siStream.close();
-				fiStream.close();
-				if (charset) {
-					data = this.toUnicode(charset, data);
-				}
-				return data;
-			} 
-			catch(e) {
-				return false;
-			}
-		},
-
-		write  : function(file, data, mode, charset) {
-			try {
-				var foStream = Components.classes[this.foutstreamCID]
-									.createInstance(this.foutstreamIID);
-				if (charset) {
-					data = this.fromUnicode(charset, data);
-				}
-				var flags = 0x02 | 0x08 | 0x20; // wronly | create | truncate
-				if (mode == 'a') {
-					flags = 0x02 | 0x10; // wronly | append
-				}
-				foStream.init(file, flags, 0664, 0);
-				foStream.write(data, data.length);
-				// foStream.flush();
-				foStream.close();
-				return true;
-			}
-			catch(e) {
-				return false;
-			}
-		},
-
-		create : function(file) {
-			try {
-				file.create(0x00, 0664);
-				return true;
-			}
-			catch(e) {
-				return false;
-			}
-		},
-
-		unlink : function(file) {
-			try {
-				file.remove(false);
-				return true;
-			}
-			catch(e) {
-				return false;
-			}
-		},
-
-		path   : function(file) {
-			try {
-				return 'file:///' + file.path.replace(/\\/g, '\/')
-							.replace(/^\s*\/?/, '').replace(/\ /g, '%20');
-			}
-			catch(e) {
-				return false;
-			}
-		},
-
-		toUnicode   : function(charset, data) {
-			try{
-				var uniConv = Components.classes[this.suniconvCID]
-									.createInstance(this.suniconvIID);
-				uniConv.charset = charset;
-				data = uniConv.ConvertToUnicode(data);
-			} 
-			catch(e) {
-				// foobar!
-			}
-			return data;
-		},
-
-		fromUnicode : function(charset, data) {
-			try {
-				var uniConv = Components.classes[this.suniconvCID]
-									.createInstance(this.suniconvIID);
-				uniConv.charset = charset;
-				data = uniConv.ConvertFromUnicode(data);
-				// data += uniConv.Finish();
-			}
-			catch(e) {
-				// foobar!
-			}
-			return data;
-		}
-
-	}
+loader.loadSubScript("chrome://cssedit/content/js/io.js");
 
 Firebug.CSSEditModel = extend(Firebug.Module, {
     initialize: function(prefDomain, prefNames) {
@@ -231,8 +104,6 @@ CSSEditPanel.prototype = extend(Firebug.Panel,
 	
 	addScripts: function(){
 		// Get jQuery in here
-		var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-            .getService(Components.interfaces.mozIJSSubScriptLoader);
 		loader.loadSubScript("chrome://cssedit/content/js/jquery-1.6.2.js", this);
 		loader.loadSubScript("chrome://cssedit/content/js/jquery.tmpl.js", this);
 		loader.loadSubScript("chrome://cssedit/content/js/jquery-ui-1.8.11.custom.min.js", this);
@@ -1084,7 +955,7 @@ CSSEditPanel.prototype = extend(Firebug.Panel,
 		// Generate css display if there were no errors
 		jQuery('#stylesheet', c.panelNode).html( jQuery.tmpl('css', {decs: c.context.stylesheet[url].styles}) ).sortable('refresh');
 		jQuery(c.panelNode).find('.wrap.filtered').removeClass('filtered');
-	
+		
 		var sort_start;
 		// Value sorting
 		jQuery('.properties', c.displayNode).sortable({
